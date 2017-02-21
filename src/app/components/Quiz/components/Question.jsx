34c091601c3e1c0e216e1c98b43cf21a0ed47f9c
Samparkyn <samparkyn101@gmail.com>
2017-02-21 20:12:31 +0000

    Fix logic for index comparison in question component

--- src/app/components/Quiz/components/Question.jsx
+++ src/app/components/Quiz/components/Question.jsx
@@ -1,7 +1,7 @@
-import React, { PropTypes, Component } from 'react';
-import { connect }                     from 'react-redux';
-import { getQuestions, nextQuestion }  from '../state/actions';
-
+import React, { PropTypes, Component }           from 'react';
+import { connect }                               from 'react-redux';
+import { getQuestions, nextQuestion, startQuiz } from '../state/actions';
+import { push, replace }                         from 'react-router-redux';
 
 export class Question extends Component {
   
@@ -10,21 +10,37 @@ export class Question extends Component {
     questions:       PropTypes.array,
     currentScore:    PropTypes.number,
     currentQuestion: PropTypes.number,
-    params:          PropTypes.object
+    params:          PropTypes.object,
+    playerName:      PropTypes.string
   }
   
   constructor(props) {
     super(props);
     this.state = {
-      selectedAnswer: -1
+      currentQuestion: 0,
+      selectedAnswer: '',
+      error: ''
     };
   }
   
+  
+  componentWillReceiveProps(nextProps) {
+    const { params } = nextProps;
+    const nextQuestionIdx = parseInt(params.id) - 1;
+    
+    if (params.id !== this.state.currentQuestion) {
+      this.setState({currentQuestion: nextQuestionIdx});
+    }
+  }
+  
+  
   componentWillMount() {
-    const { questions, dispatch } = this.props;
+    const { questions, dispatch, params } = this.props;
     if (!questions.length) {
       dispatch(getQuestions());
     }
+    
+    this.setState({currentQuestion: parseInt(params.id) - 1});
   }
   
   
@@ -33,17 +49,41 @@ export class Question extends Component {
     this.setState({selectedAnswer: value});
   }
   
+  
+  clickHandler = () => {
+    const { selectedAnswer, currentQuestion } = this.state;
+    const { dispatch, questions } = this.props;
+    
+    if (selectedAnswer === '') {
+      this.setState({error: 'Select an answer first'});
+      return;
+    }
+    
+    this.setState({error: '', selectedAnswer: ''});
+    dispatch(nextQuestion(this.state.selectedAnswer));
+    
+    if (currentQuestion < questions.length - 1) {
+      dispatch(push(`/quiz/question/${currentQuestion + 2}`));
+    } else {
+      dispatch(push('/quiz/results'));
+    }
+  }
+  
 
   render() {
-    const { questions, currentQuestion, currentScore } = this.props;
-    const { selectedAnswer } = this.state;
-
+    const { questions, currentScore } = this.props;
+    const { selectedAnswer, error, currentQuestion } = this.state;
+  
     if (!questions.length) {
       return <div>Loading questions...</div>;
     }
+    
+    let errorMsg;
+    if (error) {
+      errorMsg = <div className="error">{error}</div>;
+    }
 
     const questionObj = questions[currentQuestion];
-    
     const question = <div  className="question">{questionObj.question}</div>;
     const answerRows = questionObj.answers.map((answer, idx) => {
       const checked = selectedAnswer === idx;
@@ -66,12 +106,20 @@ export class Question extends Component {
       </div>
     );
     
-    const nextBtn = <button className="nxt-btn">Next</button>;
     const score = <p>{`Points so far: ${currentScore}`}</p>;
+    const lastQuestion = currentQuestion === questions.length - 1;
+    const nextBtn = (
+      <button
+        onClick={this.clickHandler}
+        className="nxt-btn">
+        {lastQuestion ? 'Show results' : 'Next'}
+      </button>
+    );
     
     return (
       <div className="ui-box question-container">
         {score}
+        {errorMsg}
         {question}
         {answers}
         {nextBtn}
@@ -84,7 +132,8 @@ const mapStateToProps = state => {
   return {
     questions:       state.quiz.questions,
     currentScore:    state.quiz.currentScore,
-    currentQuestion: state.quiz.currentQuestion
+    currentQuestion: state.quiz.currentQuestion,
+    playerName:      state.quiz.playerName
   };
 };
 
